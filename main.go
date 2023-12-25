18e54b8e71eaf3434adeef578ead153422d272ec
Andy Hebrank <andy.hebrank@platform.sh>
2023-12-25 10:16:29 -0500

    rearrange a little

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 
@@ -27,39 +28,46 @@ func main() {
 		log.Fatal(err)
 	}
 
-	fanClient := ecobee.NewClient(conf.EcobeeBaseUrl, conf.EcobeeApiKey, conf.EcobeeRefreshToken)
-	status, err := fanClient.Status()
-	if err != nil {
-		log.Fatal(err)
-	}
-	if status.EquipmentStatus != "" && status.EquipmentStatus != "fan" {
-		log.Fatal("equipment already running")
-	}
+	sensorOnlyFlag := flag.Bool("s", false, "Sensor output only, no fan trigger")
+	flag.Parse()
 
 	sensorClient := awair.NewClient(conf.AwairUrl)
-
 	data, err := sensorClient.Get()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// Check limits
-	if data.CO2 > conf.CO2Limit || data.VOC > conf.VocLimit || data.PM25 > conf.PmLimit {
-		fmt.Printf("Fan on. CO2: %d; VOC: %d; PM25: %d\n", data.CO2, data.VOC, data.PM25)
-		err = fanClient.FanOn(*status, status.Time, conf.FanTime)
-		if err != nil {
-			log.Fatal(err)
-		}
-	} else {
-		// Only resume if the fan is the only thing running.
-		if status.EquipmentStatus == "fan" {
-			fmt.Println("Resuming program.")
-			err = fanClient.Resume()
+	fanClient := ecobee.NewClient(conf.EcobeeBaseUrl, conf.EcobeeApiKey, conf.EcobeeRefreshToken)
+	status, err := fanClient.Status()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("CO2: %d (limit %d); VOC: %d (limit %d); PM25: %d (limit %d)\n", data.CO2, conf.CO2Limit, data.VOC, conf.VocLimit, data.PM25, conf.PmLimit)
+
+	if status.EquipmentStatus != "" && status.EquipmentStatus != "fan" {
+		log.Fatal("equipment already running")
+	}
+
+	if !*sensorOnlyFlag {
+		// Check limits
+		if data.CO2 > conf.CO2Limit || data.VOC > conf.VocLimit || data.PM25 > conf.PmLimit {
+			fmt.Println("Fan on.")
+			err = fanClient.FanOn(*status, status.Time, conf.FanTime)
 			if err != nil {
 				log.Fatal(err)
 			}
 		} else {
-			log.Fatal("EquipmentStatus is not 'fan' only; skipping program resume.")
+			// Only resume if the fan is the only thing running.
+			if status.EquipmentStatus == "fan" {
+				fmt.Println("Resuming program.")
+				err = fanClient.Resume()
+				if err != nil {
+					log.Fatal(err)
+				}
+			} else {
+				log.Fatal("EquipmentStatus is not 'fan' only; skipping program resume.")
+			}
 		}
 	}
 }
